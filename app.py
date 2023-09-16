a4c12e1d1908502e56f7f12443a9a2d83ae6d50e
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-09-16 17:40:47 +0530

    ggs

--- app.py
+++ app.py
@@ -40,7 +40,10 @@ if uploaded_file is not None:
 
     # fetching unique users
     user_list=df['user'].unique().tolist()
-    user_list.remove('group notification')
+    if 'group_notification' in user_list:
+        user_list.remove('group_notification')
+
+    # user_list.remove('group notification')
     user_list.sort()
     user_list.insert(0,"Overall")
     selected_user=st.sidebar.selectbox("Show analysis wrt",user_list)
